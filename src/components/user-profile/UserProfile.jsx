23c3450ea906736222c0af018b502173d2752e61
Miles Advani <146325158+MilesAdvani@users.noreply.github.com>
2024-08-20 08:13:14 +0200

    feat: Update CSS class names in Terms.css and InfoCard.css

--- src/components/user-profile/UserProfile.jsx
+++ src/components/user-profile/UserProfile.jsx
@@ -1,22 +1,23 @@
-import "./UserProfile.css";
-import { useAuthContext } from "../../store/authentication-context/AuthenticationContext.jsx";
+// import "./UserProfile.css";
+// import { useAuthContext } from "../../store/authentication-context/AuthenticationContext.jsx";
+// import InfoCard from "../info-card/InfoCard.jsx";
 
-const UserProfile = () => {
-  const { isLoggedIn, user } = useAuthContext(); // Use useAuthContext hook to access isLoggedIn state and user object
+// const UserProfile = () => {
+//   const { isLoggedIn, user } = useAuthContext(); // Use useAuthContext hook to access isLoggedIn state and user object
 
-  return (
-    <>
-      {isLoggedIn && (
-        <div className="user-info">
-          <p>Email: {user.email}</p>
-          <p>Name: {user.userName}</p>
-          <p>Role: {user.role}</p>
-          <p>Id: {user.id}</p>
-          <img src={user.image} alt="User" />
-        </div>
-      )}
-    </>
-  );
-};
+//   return (
+//     <>
+//       {isLoggedIn && (
+//         <div className="user-info">
+//           <p>Email: {user.email}</p>
+//           <p>Name: {user.userName}</p>
+//           <p>Role: {user.role}</p>
+//           <p>Id: {user.id}</p>
+//           <img src={user.image} alt="User" />
+//         </div>
+//       )}
+//     </>
+//   );
+// };
 
-export default UserProfile;
+// export default UserProfile;
