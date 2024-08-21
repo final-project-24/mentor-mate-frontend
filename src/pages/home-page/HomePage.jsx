648b4370fe682b93a1619ad29f9dadbe80a42da8
Ignacio Guzman <ignacioguzmanok@gmail.com>
2024-08-21 14:01:18 +0200

    WIP: resolved conflicts in HomePage.jsx and other changes

--- src/pages/home-page/HomePage.jsx
+++ src/pages/home-page/HomePage.jsx
@@ -1,16 +1,55 @@
 import "./HomePage.css";
 import Layout from "../../components/layout/Layout";
 import ReviewSidebar from "../review-sidebar/ReviewSidebar";
+import { useAuthContext } from "../../store/authentication-context/AuthenticationContext";
 
 const HomePage = () => {
+  const { user, isLoggedIn } = useAuthContext();
+
+  const getWelcomeMessage = () => {
+    if (!isLoggedIn) {
+      return "Welcome to MentorMate";
+    } else if (user.role === "mentor") {
+      return "Welcome Mentor";
+    } else if (user.role === "mentee") {
+      return "Welcome Mentee";
+    }
+  };
+
+  const userNameDisplay = () => {
+    if (!isLoggedIn) {
+      return "Your learning platform";
+    } else if (user.role === "mentor" || user.role === "mentee") {
+      return user.userName;
+    }
+  };
+
   return (
     <Layout>
-      <section id="home">
-        <p>
-          Home: This is where the user will land when they first visit the
-          application.
+      <section id="home" className="pt-[150px] mx-20">
+        <h1 className="text-3xl text-center text-accent">
+          {getWelcomeMessage()}
+        </h1>
+        <p className="text-2xl text-center text-neutral py-4">
+          {userNameDisplay()}
         </p>
+        {/* Keep ReviewSidebar from nacho branch */}
         <ReviewSidebar />
+        {/* Keep comments section from main branch */}
+        <section className="flex py-10">
+          <div className="border border-red-500 w-1/4 h-[400px] mx-2">
+            Some random comments about platform
+          </div>
+          <div className="border border-red-500 w-1/4 h-[400px] mx-2">
+            Some random comments about platform
+          </div>
+          <div className="border border-red-500 w-1/4 h-[400px] mx-2">
+            Some random comments about platform
+          </div>
+          <div className="border border-red-500 w-1/4 h-[400px] mx-2">
+            Some random comments about platform
+          </div>
+        </section>
       </section>
     </Layout>
   );
