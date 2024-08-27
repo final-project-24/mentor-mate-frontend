84217bc9b4711e2bc07a5d3863b004ea2822c321
Milos Mirkovic <milos.mirkovic7@gmail.com>
2024-08-27 10:25:45 +0200

    fixing error in Login.jsx

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,26 +1,25 @@
 import React, { useState } from "react";
 import "./Login.css";
-import { useAuthContext } from "../../store/authentication-context/AuthenticationContext.jsx"; //
-import { useNavigate } from "react-router-dom"; //
+import { useAuthContext } from "../../store/authentication-context/AuthenticationContext.jsx";
+import { useNavigate } from "react-router-dom";
 
 function Login({ onLogin }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
 
-  const { login } = useAuthContext(); // Use useAuthContext hook to access login method
-
-  const navigate = useNavigate(); //
+  const { login } = useAuthContext(); /
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      await login(email, password); // Use login method from authentication context
+      await login(email, password); 
 
       setEmail("");
       setPassword("");
-      navigate("/"); //
+      navigate("/"); 
     } catch (error) {
       console.error("Login failed:", error);
       setErrorMessage("Login failed. Please try again.");
@@ -29,7 +28,7 @@ function Login({ onLogin }) {
 
   return (
     <div className="pt-[80px] lg:pt-[100px]">
-      <div className="container ">
+      <div className="container">
         <div className="form-container">
           <h2>Login</h2>
           {errorMessage && <p className="error-message">{errorMessage}</p>}
