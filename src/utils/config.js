9b378aac18827ef63dfefc3023a9339686952952
Jakub <derivatives_four@protonmail.com>
2024-09-05 11:53:46 +0200

    feat: add `logIfNodeDev` util
    - This util can be used to produce logs only when the `VITE_NODE_ENV` is set to 'dev'.
    - Added `.env.example` with instructions on how to use the `VITE_NODE_ENV` variable.
    - Added `VITE_NODE_ENV` variable to `config.js`

--- src/utils/config.js
+++ src/utils/config.js
@@ -6,7 +6,10 @@ export const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL || "http://localhost:4000/app";
 
 export const STRIPE_PUBLIC_KEY =
- import.meta.env.VITE_STRIPE_PUBLIC_KEY;
+  import.meta.env.VITE_STRIPE_PUBLIC_KEY;
 
 export const PAYPAL_CLIENT_ID =
- import.meta.env.PAYPAL_CLIENT_ID;
+  import.meta.env.PAYPAL_CLIENT_ID;
+
+export const NODE_ENV =
+  import.meta.env.VITE_NODE_ENV
