a7f928db661cfdd0873b85ef8f8d3392562754be
Jakub <derivatives_four@protonmail.com>
2024-09-06 11:21:23 +0200

    refactor: improved the component
    - Added `RenderErrorArray` component to show errors returned by BE validators.

--- src/components/skills/skill-category-form/SkillCategoryForm.jsx
+++ src/components/skills/skill-category-form/SkillCategoryForm.jsx
@@ -1,5 +1,5 @@
 import { useDispatch } from "react-redux"
-import { set_skill_category_description, set_skill_category_title } from "../../../store/skills-store/slices/forms/skillCategoryFormSlice"
+import { set_skill_category_description, set_skill_category_title } from "../../../store/skills-store/slices/skillCategoryFormSlice"
 
 // hooks
 import useStateSelectors from "../../../hooks/useStateSelectors"
@@ -8,6 +8,8 @@ import useApiConnectors from "../../../hooks/useApiConnectors"
 // components
 import ButtonCloseForm from "../buttons/ButtonCloseForm"
 import ButtonSubmitForm from "../buttons/ButtonSubmitForm"
+import RenderErrorArray from "../render-error-array/RenderErrorArray"
+import { set_errors_array } from "../../../store/skills-store/slices/errorsSlice"
 
 const SkillCategoryForm = () => {
   const dispatch = useDispatch()
@@ -21,29 +23,33 @@ const SkillCategoryForm = () => {
     addForm
       ? createSkillCategory()
       : editSkillCategory()
+    dispatch(set_errors_array([]))
   }
 
   return (
     <form 
-      className="relative flex flex-col gap-3 px-2 pb-3 pt-12 rounded-md bg-stone-300"
+      className="relative flex flex-col gap-8 px-2 pb-3 pt-12 rounded-md bg-stone-300"
       onSubmit={onSubmit}
     >
-      <input
-        type="text"
-        className={inputClass}
-        onChange={e => dispatch(set_skill_category_title(e.target.value))}
-        value={skillCategoryForm.skillCategoryTitle}
-        placeholder="Category title"
-      />
-      <input 
-        type="text"
-        className={inputClass}
-        onChange={e => dispatch(set_skill_category_description(e.target.value))}
-        value={skillCategoryForm.skillCategoryDescription}
-        placeholder="Category description"
-      />
+      <div className="flex flex-col gap-3">
+        <input
+          type="text"
+          className={inputClass}
+          onChange={e => dispatch(set_skill_category_title(e.target.value))}
+          value={skillCategoryForm.skillCategoryTitle}
+          placeholder="Skill Category Title"
+        />
+        <input
+          type="text"
+          className={inputClass}
+          onChange={e => dispatch(set_skill_category_description(e.target.value))}
+          value={skillCategoryForm.skillCategoryDescription}
+          placeholder="Skill Category Description"
+        />
+      </div>
       <ButtonCloseForm />
-      <div className="flex justify-center mt-3">
+      <RenderErrorArray />
+      <div className="flex justify-center">
         <ButtonSubmitForm />
       </div>
     </form>
