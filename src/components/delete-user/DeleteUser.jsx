febb00cab696c850475d7560a0833e7f75b09183
Miles Advani <146325158+MilesAdvani@users.noreply.github.com>
2024-09-11 17:57:32 +0200

    chore: Add confirmation dialog for user deletion

--- src/components/delete-user/DeleteUser.jsx
+++ src/components/delete-user/DeleteUser.jsx
@@ -1,9 +1,12 @@
+import React, { useState } from "react";
 import { deleteUser } from "../../utils/api-connector";
 import { useLanguageContext } from "../../store/language-context/LanguageContext";
 import "./DeleteUser.css";
+import ConfirmationDialog from "../confirmation-dialog/ConfirmationDialog";
 
 const DeleteUser = () => {
   const { settingsData } = useLanguageContext(); // Use settingsData from LanguageContext
+  const [showDialog, setShowDialog] = useState(false);
 
   const handleDeleteUser = async () => {
     try {
@@ -14,16 +17,38 @@ const DeleteUser = () => {
     }
   };
 
+  //   return (
+  //     <button
+  //       className="delete-user-button"
+  //       onClick={() => {
+  //         console.log("Attempting to delete user");
+  //         handleDeleteUser();
+  //       }}
+  //     >
+  //       {settingsData.deleteUserButtonLabel}
+  //     </button>
+  //   );
+  // };
+
   return (
-    <button
-      className="delete-user-button"
-      onClick={() => {
-        console.log("Attempting to delete user");
-        handleDeleteUser();
-      }}
-    >
-      {settingsData.deleteUserButtonLabel}
-    </button>
+    <div>
+      <button
+        className="delete-user-button"
+        onClick={() => {
+          console.log("Attempting to delete user");
+          setShowDialog(true);
+        }}
+      >
+        {settingsData.deleteUserButtonLabel}
+      </button>
+      {showDialog && (
+        <ConfirmationDialog
+          message="Are you sure you want to delete this user?"
+          onConfirm={handleDeleteUser}
+          onCancel={() => setShowDialog(false)}
+        />
+      )}
+    </div>
   );
 };
 
