535e7f69056f7369cdc095e7739988cc644dac96
Ignacio Guzman <ignacioguzmanok@gmail.com>
2024-09-12 11:49:04 +0200

    update contact

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -3,28 +3,17 @@ import Layout from "../../components/layout/Layout";
 import "./Contact.css";
 
 
-
-// const AboutUs = () => {
-//   return (
-//     <Layout>
-        
-      
-//     </Layout>
-//   );
-// };
-// export default AboutUs;
-
 import React from 'react';
 
 const ContactForm = () => {
   return (
     <Layout>
-    <div className="max-w-screen-lg mx-auto p-5">
+    <div className="max-w-screen-lg mx-auto p-5 mt-20">
       <div className="grid grid-cols-1 md:grid-cols-12 border">
-        <div className="bg-gray-900 md:col-span-4 p-10 text-white">
+        <div className="bg-green-500 md:col-span-4 p-10 text-white">
           <p className="mt-4 text-sm leading-7 font-regular uppercase">Contact</p>
           <h3 className="text-3xl sm:text-4xl leading-normal font-extrabold tracking-tight">
-            Get In <span className="text-indigo-600">Touch</span>
+            Get In <span className="text-gray-700 ">Touch</span>
           </h3>
           <p className="mt-4 leading-7 text-gray-200">
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
@@ -32,16 +21,16 @@ const ContactForm = () => {
           </p>
 
           <div className="flex items-center mt-5">
-            <svg className="h-6 mr-2 text-indigo-600" fill="currentColor" version="1.1"
+            <svg className="h-6 mr-2 text-gray-700 " fill="currentColor" version="1.1"
               xmlns="http://www.w3.org/2000/svg" viewBox="0 0 489.536 489.536">
               <path d="M488.554,476l-99-280.2c-1-4.2-5.2-7.3-9.4-7.3h-45.6c12.9-31.1 19.6-54.9 19.6-70.8 0-64.6-50-117.7-112.5-117.7-61.5,0-112.5,52.1-112.5,117.7 0,17.6 8.2,43.1 19.9,70.8h-39.7c-4.2,0-8.3,3.1-9.4,7.3l-99,280.2c-3.2,10.3 6.3,13.5 9.4,13.5h468.8c4.2,0.5 12.5-4.2 9.4-13.5zm-246.9-455.3c51,1.06581e-14 91.7,43.7 91.7,96.9 0,56.5-79.2,182.3-91.7,203.1-31.3-53.1-91.7-161.5-91.7-203.1 0-53.1 40.6-96.9 91.7-96.9zm-216.7,448l91.7-259.4h41.7c29.9,64.1 83.3,151 83.3,151s81.4-145.7 83.8-151h47.4l91.7,259.4h-439.6z"></path>
               <rect width="136.5" x="177.054" y="379.1" height="20.8"></rect>
               <path d="M289.554,108.2c0-26-21.9-47.9-47.9-47.9s-47.9,21.9-47.9,47.9 20.8,47.9 47.9,47.9c27.1,0 47.9-21.8 47.9-47.9zm-75-1c0-14.6 11.5-27.1 27.1-27.1s27.1,12.5 27.1,27.1-11.5,27.1-27.1,27.1c-14.6,2.84217e-14-27.1-12.5-27.1-27.1z"></path>
             </svg>
-            <span className="text-sm">House #14, Street #12, Darulaman Road, Kabul, Afghanistan.</span>
+            <span className="text-sm"> Street 742, Evergreen Terrace, Berlin, Germany.</span>
           </div>
           <div className="flex items-center mt-5">
-            <svg className="h-6 mr-2 text-indigo-600" fill="currentColor" version="1.1" id="Capa_1"
+            <svg className="h-6 mr-2 text-gray-700 " fill="currentColor" version="1.1" id="Capa_1"
               xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 60.002 60.002">
               <path d="M59.002,37.992c-3.69,0-6.693-3.003-6.693-6.693c0-0.553-0.447-1-1-1s-1,0.447-1,1c0,4.794,3.899,8.693,8.693,8.693
                 c0.553,0,1-0.447,1-1S59.554,37.992,59.002,37.992z"></path>
@@ -71,7 +60,7 @@ const ContactForm = () => {
             <span className="text-sm">+93 749 99 65 50</span>
           </div>
           <div className="flex items-center mt-5">
-            <svg className="h-6 mr-2 text-indigo-600" fill="currentColor" version="1.1" id="Capa_1"
+            <svg className="h-6 mr-2 text-gray-700 " fill="currentColor" version="1.1" id="Capa_1"
               xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300.988 300.988">
               <path d="M150.494,0.001C67.511,0.001,0,67.512,0,150.495s67.511,150.493,150.494,150.493s150.494-67.511,150.494-150.493
                 S233.476,0.001,150.494,0.001z M150.494,285.987C75.782,285.987,15,225.206,15,150.495S75.782,15.001,150.494,15.001
@@ -134,15 +123,15 @@ const ContactForm = () => {
                 </label>
               </div>
               <button
-                className="shadow bg-indigo-600 hover:bg-indigo-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-6 rounded"
+                className="shadow bg-green-500 hover:bg-green-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-6 rounded"
                 type="submit"
               >
                 Send Message
               </button>
             </div>
-            <a href="https://veilmail.io/e/FkKh7o" className="font-medium text-blue-600 hover:underline">
+            {/* <a href="https://veilmail.io/e/FkKh7o" className="font-medium text-blue-600 hover:underline">
               Or click here to reveal our protected email address
-            </a>
+            </a> */}
           </div>
         </form>
       </div>
