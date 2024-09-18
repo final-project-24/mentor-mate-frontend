04a568ce4faac5d2205b72b4e66f570988ff466c
Milos Mirkovic <milos.mirkovic7@gmail.com>
2024-09-18 11:37:27 +0200

    fixing nav and login button

--- src/components/login-button/LoginButton.jsx
+++ src/components/login-button/LoginButton.jsx
@@ -1,11 +1,23 @@
-// import React from "react";
-import { Link } from "react-router-dom";
+import { useAuthContext } from "../../store/authentication-context/AuthenticationContext.jsx";
+import { useNavigate } from "react-router-dom";
 import "./LoginButton.css";
 
 const LoginButton = () => {
+  const { login } = useAuthContext(); // Access the login method
+  const navigate = useNavigate(); // Use React Router's navigate function
+
+  const handleLogin = async () => {
+    try {
+      await login(); // Call login method from the context
+      navigate("/authentication"); // Navigate to the authentication page after login
+    } catch (error) {
+      console.error("Error logging in:", error);
+    }
+  };
+
   return (
     <div className="login-button-container">
-      <Link to="/authentication">Login</Link>
+      <button onClick={handleLogin} className="items-center justify-end">Login</button>
     </div>
   );
 };
